7f5d5548b51cb7895682cbdd93487de9b0bb3114
unjerry <2351688512@qq.com>
2024-08-29 09:25:38 +0800

    DevNeo_fft

--- aiTest/fft1/stk.py
+++ aiTest/fft1/stk.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 np.set_printoptions(linewidth=np.inf)
 fig = plt.figure(figsize=(100, 5), dpi=300)
 
-df = pd.read_excel("data/500.xlsx", usecols=[0, 14])
+df = pd.read_excel("data/600529gai.xlsx", usecols=[0, 1])
 df["date"] = pd.to_datetime(df["date"], format="%Y%m%d")
 print("dataframe ", df, sep="\n")
 
-plt.scatter(df["date"], df["pb_earning_probability"])
+plt.scatter(df["date"], df["zuli5_zoom_ma"])
 fig.savefig("pe.png")
 
 """ main real value fft proccess """
-fft_values = np.fft.fft(df["pb_earning_probability"])
+fft_values = np.fft.fft(df["zuli5_zoom_ma"])
 frequencies = np.fft.fftfreq(
-    n=len(df["pb_earning_probability"]), d=1 / len(df["pb_earning_probability"])
+    n=len(df["zuli5_zoom_ma"]), d=1 / len(df["zuli5_zoom_ma"])
 )
 fig = plt.figure(figsize=(20, 10), dpi=300)
 plt.stem(frequencies, np.abs(fft_values))
@@ -23,6 +23,13 @@ plt.xlabel("Frequency")
 plt.ylabel("Amplitude")
 plt.title("Fourier Transform Spectrum")
 fig.savefig("fft.png")
+
+fig = plt.figure(figsize=(40, 10), dpi=300)
+plt.stem(frequencies[0:600], np.abs(fft_values[0:600]))
+plt.xlabel("Frequency")
+plt.ylabel("Amplitude")
+plt.title("Fourier Transform Spectrum")
+fig.savefig("fft100.png")
 
 fig = plt.figure(figsize=(20, 10), dpi=300)
 plt.stem(frequencies, np.angle(fft_values))
@@ -32,19 +39,19 @@ plt.title("Fourier Transform Spectrum")
 fig.savefig("fftangle.png")
 
 
-fig = plt.figure(figsize=(20, 10), dpi=300)
+fig = plt.figure(figsize=(100, 10), dpi=300)
 T = np.arange(0, 1.2, (1 / (frequencies.size)))
-Y = np.ones_like(T) * np.abs(fft_values)[0] / len(df["pb_earning_probability"])
+Y = np.ones_like(T) * np.abs(fft_values)[0] / len(df["zuli5_zoom_ma"])
 print(Y[0])
-for k in range(1, 3):
+for k in range(100, 300):
     Y += np.real(
         np.exp(2j * np.pi * T * k) * fft_values[k] / frequencies.size
         + np.exp(2j * np.pi * T * (-k)) * fft_values[-k] / frequencies.size
     )
 
 plt.scatter(
-    np.arange(0, df["pb_earning_probability"].size, 1), df["pb_earning_probability"]
+    np.arange(0, df["zuli5_zoom_ma"].size, 1), df["zuli5_zoom_ma"]
 )
 plt.scatter(np.arange(0, Y.size, 1), Y)
-print(T.size, df["pb_earning_probability"].size, frequencies.size)
-fig.savefig("pe_with_wave3.png")
+print(T.size, df["zuli5_zoom_ma"].size, frequencies.size)
+fig.savefig("pe_with_wave30.png")
